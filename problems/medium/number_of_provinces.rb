af3ef5b7819fa2352e9e6c14f252dd871f55474b
Ahmed A. Ibrahim <ahmed.saraya90@gmail.com>
2021-09-30 10:33:18 +0200

    format number_of_ provinces

--- problems/medium/number_of_provinces.rb
+++ problems/medium/number_of_provinces.rb
@@ -3,8 +3,8 @@
 def find_circle_num(is_connected)
   count = 0
   visited = Array.new(is_connected.size, false)
-  for i in 0...is_connected.size
-    if !visited[i]
+  (0...is_connected.size).each do |i|
+    unless visited[i]
       dfs(is_connected, i, visited)
       count += 1
     end
@@ -14,10 +14,7 @@ end
 
 def dfs(is_connected, i, visited)
   visited[i] = true
-  for j in 0...is_connected.size
-    next if j == i
-    if is_connected[i][j] == 1 && !visited[j]
-      dfs(is_connected, j, visited)
-    end
+  (0...is_connected.size).each do |j|
+    dfs(is_connected, j, visited) if is_connected[i][j] == 1 && !visited[j]
   end
 end
