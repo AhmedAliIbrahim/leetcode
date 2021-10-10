cbd68bdef33ff3c956d8e0486d43e6901a174c99
Ahmed A. Ibrahim <ahmed.saraya90@gmail.com>
2021-10-10 09:43:49 +0200

    Time Out of Three

--- problems/easy/time_out_of_three.rb
+++ problems/easy/time_out_of_three.rb
@@ -4,17 +4,17 @@
 # @return {Integer[]}
 def two_out_of_three(nums1, nums2, nums3)
   map = Hash.new(0)
-  for num in nums1.uniq
+  nums1.uniq.each do |num|
     map[num] += 1 if map[num].zero?
   end
-  
-  for num in nums2.uniq
+
+  nums2.uniq.each do |num|
     map[num] += 1
   end
-  
-  for num in nums3.uniq
+
+  nums3.uniq.each do |num|
     map[num] += 1
   end
-  
-  map.select{|k, v| v >= 2}.keys
+
+  map.select { |_k, v| v >= 2 }.keys
 end
